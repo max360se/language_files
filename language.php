d6aa711fd2890369cca987a2eea9ff685ff1d48a
Mario Borna Mjertan <bor.mbm@gmail.com>
2013-02-05 05:42:42 -0800

    The first one

---
+++ language.php
@@ -0,0 +1,88 @@
+<?php
+
+$lang_hr = array(
+// This isn't a complete list of all messages in Pulsir, just a few and it isn't implemented yet. 
+// frontpage start
+// frontpage intro start
+'frontpage_heading' = 'Pulsir je bolji, brži i jednostavniji način za bloganje.';
+'frontpage_intro_text' = 'Napišite svoj najbolji tekst i prepustite tehnikalije nama. Pratite svoje omiljene blogere i sudjelujte u raspravama. ';
+'frontpage_addpost' = 'Dodajte svoj prvi post';
+'frontpage_addpost_today' = 'već danas ili';
+'frontpage_getaprofile' = 'otvorite profil';
+'frontpage_notawriter' = 'Ako niste raspoloženi za pisanje, ';
+'frontpage_newposts' = 'pogledajte što je novo';
+// frontpage intro stop
+// frontpage getaprofile-box start
+'frontpage_getaprofile-box_heading' = 'Nabavite svoj profil';
+// this can be global, so let's make it global
+'email_adress' = 'Email adresa';
+'username' = 'Korisničko ime';
+'moreinfo' = 'Više informacija';
+// end of global stuff
+'frontpage_getaprofile-box_button' = 'Registrirajte se';
+'frontpage_getaprofile-wontbeshared'= 'Vaša e-mail adresa neće biti dijeljena.';
+// end of the getaprofile-box
+'frontpage_needhelp_heading' = 'Trebate pomoć?'
+'frontpage_needhelp_text' = 'Javite nam se! Rado ćemo odgovoriti na sve vaše upite.';
+'frontpage_needhelp_tenderopen' = 'Otvorite novo pitanje';
+// this also can be global
+'or' = 'ili';
+// no more global
+'frontpage_needhelp_tenderkb' = 'posjetite našu bazu znanja';
+// the menu here
+'menu_newposts' = 'Novi postovi'; 
+'menu_tos' = 'Uvjeti korištenja';
+'menu_privacy' = 'Privatnost';
+'menu_support' = 'Podrška';
+'menu_team' = 'Tim';
+// some messages
+'usesandrecommends' = 'Pulsir koristi i preporučuje';
+'ads' = 'Oglasi:';
+// terms of use
+'tos' = 'Kako bismo na Pulsiru održali red i diskusije na prihvatljivom nivou, molimo Vas da se pridržavate uvjeta korištenja. Na Pulsiru je zabranjeno:
+<ul><li>kršenje zakona Republike Hrvatske (posebno Vam skrećemo pažnju na pozivanje na mržnju ili diskriminaciju, opravdavanje ili veličanje kaznenih dijela, kršenje autorskih prava, te pedofiliju, mučenje životinja i sl.)</li>
+<li>reklamiranje usluga, proizvoda i komercijalnih firmi postanje nepotrebnih i nepoželjnih informacija, informacija nevezanih za temu ili postanje na način koji smanjuje čitljivost foruma (uključuje i neprimjerene naslove)</li>
+<li>ometanje rada Pulsira (ignoriranje upozorenja, vraćanje izbrisanih tekstova, razbijanje servera :D)<(li>
+<li>vrijeđanje sugovornika, namjerno provociranje svađe na osobnom nivou, nekonstruktivnih nacionalnih, vjerskih i sličnih sukoba (trollanje)</li>
+<li>objavljivanje privatnih podataka drugih korisnika<li></ul>
+Svi postovi su mišljenja njihovih autora, a ne samog Pulsira, njegovih moderatora, VlexoFree hostinga, kao ni autora Minima platforme. 
+Oni se ne mogu smatrati odgovornima za njihov sadržaj. 
+Moderatori rade na tome da se nedozvoljeni postovi sankcioniraju, no možda će neke postove previdjeti ili ih neće uočiti na vrijeme. Molimo Vas da imate razumijevanja, i, ako želite pomoći, slobodni ste svaki post za koji smatrate da krši pravila prijaviti moderatorima na report@plsr.tk
+<br>
+Zahvaljujemo na pažnji i želimo vam ugodan boravak na Pulsiru.'
+// privacy policy
+'privacy' = '<title>Privatnost na Pulsiru - Politika privatnosti</title>
+</head>
+<body>
+<center><h1>Politika privatnosti<span class="per">.</span></h1><h3>Naša osnovna <span class="per">pravila</span></h3><p><span class="per">1.</span> Sve što napišete na Pulsiru je automatski vidljivo na popisu novih objava (feedu) i dostupno je preko API-a te Vašeg profila <br><br><br><span class="per">2.</span> Prikupljamo tek one podatke koji su nam bitni za unaprijeđenje usluga, no ne možemo ih povezati s osobom, poput lokacije (u rasponu od pedesetak kilometara), IP adrese, operativnog sustava i jezika, za što koristimo <a href="http://gosquared.com">GoSquared</a>, <a href="http://google.com/analytics">Google Analytics</a> i <a href="http://clicky.com">Clicky</a>. Te podatke nećemo (i ne možemo) dijeliti osobama van Pulsir tima. Svakoj osobi koja ga napusti, oduzima se pristup tim alatima.<br><br><br><br><span class="per">3.</span> Apsolutno nikad <span class="per">*</span> nećemo dijeliti Vaše osobne podatke, uključujući i email adresu. <br><br> <span class="per">*</span> osim u slučaju sudskog zahtjeva</center>
+
+<center><h3>Imate pitanja? <strong><a href="mailto:dev@plsr.tk">Kontaktirajte nas<span class="per">.</span></a></strong></h3></center>';
+
+// add a post
+'add_h1' = 'Dodaj novi post.';
+'add_title' = 'Kako će se zvati ovaj tekst?';
+'add_title_field' = 'Naslov posta';
+'add_text' = 'Odradi svoje.';
+'add_text_field' = 'Tekst posta';
+'add_author' = 'Tko si ti?';
+'add_button' = 'Završimo ovu priču!';
+// random crap I just thought of
+'background_photo' = 'Pozadinska fotografija:'; 
+// messages
+'somethings_missing' = 'Neki traženi podatak nedostaje.';
+'try_again' = 'Pokušajte ponovno';
+'add_itworked' = 'Uspješno dodano. Vaša objava čeka moderaciju, a nakon što je uspješno odobrena, pokazat će se na popisu novih objava.';
+'cantupdate_youscum' = 'Sorry, ali još si wannabe haxxor. Nećemo ti samo tako dati prava za update sadržaja. <br>-- Hugs and killalls, Pulsir.';
+// fields
+'post_author_field' = 'Autor: ';
+'post_permalink' = 'Poveznica na objavu';
+'post_comments' = 'Komentari';
+'posts_new_title' = 'Upravo dodano';
+'post_author_by' = 'post objavio ';
+// get a profile landing
+'getaprofile_landing_h1' = 'Nabavite svoj profil';
+'getaprofile_landing_text' = 'Profili su sada otvoreni za javnost. Nabavite svoj već danas.';
+'getaprofile_landing_button' = 'Registrirajte se';
+);
+
+?>
